bfe335d2fa51f652db44432667ad09a722c900f5
Kenzy Ragab <165553300+Kenzy-Ragab@users.noreply.github.com>
2025-09-12 23:18:06 +0300

    Create ReportConsoleTitle.cs

---
+++ Screens/Reports/ConsoleUI/ReportConsoleTitle.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace GYM_System.Helper
+{
+    public static class ReportConsoleTitle
+    {
+        public static void PrintTitle(string title)
+        {
+            Console.Clear();
+            Console.WriteLine("==========================================");
+            int width = 42;
+            int padding = (width - title.Length) / 2;
+            Console.WriteLine(new string(' ', padding) + title);
+            Console.WriteLine("==========================================");
+        }
+    }
+}
