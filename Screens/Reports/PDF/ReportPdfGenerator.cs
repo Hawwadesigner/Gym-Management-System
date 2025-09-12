fa650cbd50bf23feb73cff581805dc2f17fb2792
Kenzy Ragab <165553300+Kenzy-Ragab@users.noreply.github.com>
2025-09-12 22:47:36 +0300

    Update ReportPdfGenerator.cs

--- Screens/Reports/PDF/ReportPdfGenerator.cs
+++ Screens/Reports/PDF/ReportPdfGenerator.cs
@@ -14,22 +14,27 @@ namespace GYM_System.Screens.Reports.PDF
     {
         public static void ExportReportToPdf<T>(string title, IEnumerable<T> data, Action<ColumnDescriptor,IEnumerable<T>> renderContent)
         {
-            // Create
-            var doc = Document.Create(container =>
-            {
-                container.Page(page =>
-                {
-                    page.Margin(20);
-                    page.Header().Text(title).FontSize(18).Bold().AlignCenter();
-                    page.Content().Column(col => { renderContent(col, data); });
-                    page.Footer().AlignCenter().Text(txt => { txt.Span("Genereted on ").Italic(); txt.Span(DateTime.Now.ToString("yyyy/MM/dd")); });
-                });
-            });
+                // Set the license
+                QuestPDF.Settings.License = LicenseType.Community;
 
-            // Path
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(),$"{title.Replace(" ", "_")}.pdf");
-            doc.GeneratePdf(filePath);
-            Console.WriteLine($"\n\nPDF Genereted : {filePath}");
+                // Create
+                var doc = Document.Create(container =>
+                {
+                    container.Page(page =>
+                    {
+                        page.Margin(20);
+                        page.Header().Text(title).FontSize(18).Bold().AlignCenter();
+                        page.Content().Column(col => { renderContent(col, data); });
+                        page.Footer().AlignCenter().Text(txt => { txt.Span("Genereted on ").Italic(); txt.Span(DateTime.Now.ToString("yyyy/MM/dd")); });
+                    });
+                });
+
+                // Path
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), $"{title.Replace(" ", "_")}.pdf");
+                doc.GeneratePdf(filePath);
+                Console.WriteLine($"\n\nPDF Genereted : {filePath}");
+            }
+            catch (Exception ex) { Console.WriteLine($"\n\nError generating PDF: {ex.Message}"); }
         }
     }
 }
